004fe66ce3a8babe6f8c83a492577a28d204f65f
Michael Wilson <mawilson0410@gmail.com>
2025-06-13 19:10:23 -0400

    vite analysis

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import "dotenv/config";
-import cors from "cors";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import chatRoutes from "./routes/chat.route.js";
-import { connectDB } from "./lib/db.js";
 
-import path from "path";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT;
